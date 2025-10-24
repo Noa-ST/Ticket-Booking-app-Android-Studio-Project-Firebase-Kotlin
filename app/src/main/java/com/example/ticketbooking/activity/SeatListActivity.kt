18e6123cf7db74d9b48b8417469f43dcc3949dcd
HIENNTP\hienn <hienntpse173056@fpt.edu.vn>
2025-10-24 16:25:11 +0700

    Date list

--- app/src/main/java/com/example/ticketbooking/activity/SeatListActivity.kt
+++ app/src/main/java/com/example/ticketbooking/activity/SeatListActivity.kt
@@ -5,17 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ticketbooking.R
+import com.example.ticketbooking.adapter.DateAdapter
+import com.example.ticketbooking.databinding.ActivitySeatListBinding
+import com.example.ticketbooking.model.Film
+import java.time.format.DateTimeFormatter
+import java.time.LocalDate
 
 class SeatListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySeatListBinding
+    private lateinit var film: Film
+    private var price: Double=0.0
+    private var number: Int=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_seat_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivitySeatListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        getIntentExtra()
+        setVariable()
+        initTimeDatelist()
+    }
+    private fun initTimeDatelist() {
+        binding.apply {
+            LinearLayoutManager (this@SeatListActivity, LinearLayoutManager.HORIZONTAL, false)
+            dateRecyclerview.adapter= DateAdapter(generateDates())
+        }
+    }
+    private fun setVariable() {
+        binding.backBtn.setOnClickListener { finish()}
+    }
+    private fun getIntentExtra() {
+        film=intent.getSerializableExtra("film") as Film
+    }
+    private fun generateDates(): List<String>{
+        val dates=mutableListOf<String>()
+        val today= LocalDate.now()
+        val formatter= DateTimeFormatter.ofPattern("EEE/dd/MMM")
+
+        for(i in 0 until 7){
+            dates.add(today.plusDays(i.toLong()).format(formatter))
+        }
+        return dates
     }
 }
--- app/src/main/java/com/example/ticketbooking/model/Cast.kt
+++ app/src/main/java/com/example/ticketbooking/model/Cast.kt
@@ -1,6 +1,11 @@
 package com.example.ticketbooking.model
 
+import java.io.Serializable
+
 data class Cast(
     var Picurl: String?=null,
-    var Actor: String?=null
-    )
+    var Actor: String?=null,
+    val name: String,
+    val role: String,
+    val image: String
+    ): Serializable
