0a867d349ab50ba382f80c931631fd1b41c3a09d
HIENNTP\hienn <hienntpse173056@fpt.edu.vn>
2025-10-28 19:24:27 +0700

    Edit Splash activity, Top header, Slider, Top movies and Upcoming

--- app/src/main/java/com/example/ticketbooking/model/Cast.kt
+++ app/src/main/java/com/example/ticketbooking/model/Cast.kt
@@ -3,9 +3,9 @@ package com.example.ticketbooking.model
 import java.io.Serializable
 
 data class Cast(
-    var Picurl: String?=null,
-    var Actor: String?=null,
-    val name: String,
-    val role: String,
-    val image: String
+    var Picurl: String? = null,
+    var Actor: String? = null,
+    val name: String? = null,
+    val role: String? = null,
+    val image: String? = null,
     ): Serializable
